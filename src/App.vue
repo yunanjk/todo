d7d1153ca465d5ad7e89984ae0632971588cfbf0
oyy <oyy0821@chunjae.co.kr>
2023-11-21 15:31:57 +0900

    todo list (1)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-
+  <div class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input v-model="todoText" @keyup.enter="addTodo" type="text" class="w-100 p-2" placeholder="Type todo">
+    <hr>
+    <Todo v-for="(todo, index) in todos" :key="index" :todo="todo" />
   </div>
 </template>
 
 <script>
-export default {
+import Todo from "@/components/Todo.vue";
 
+export default {
+  name: "App",
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: "buy a car", checked: true },
+        { id: 2, text: "play game", checked: false }
+      ],
+      todoText: ""
+    }
+  },
+  methods: {
+    addTodo(e) {
+      // console.log(e);
+      // console.log(e.target.value);
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = "";
+    }
+  }
 }
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="checkbox" :checked="todo.checked">
+        <span class="m-3">{{ todo.text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoComp",
+    props: {
+        todo: {
+            type: Object,
+            requried: true
+        }
+    }
+}
+</script>
