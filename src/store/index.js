9dee4b1be2ae2bba50cc3c49e52ac21ed744718e
oyy <oyy0821@chunjae.co.kr>
2023-11-22 10:10:12 +0900

    vuex 설치 및 세팅

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+export default createStore({
+    state() {
+        return {
+            // data 
+        }
+    },
+    mutations: {
+        // mutations를 통해서만 데이터 변경 가능
+    },
+    actions: {
+        // method(함수)
+    },
+    getters: {
+        // computed(계산)
+    }
+});
